5ce483c658862188992920a0d10f7277b4b84d71
melissa billard <billardmelissa1@gmail.com>
2024-09-08 19:00:29 +0200

    update v

--- app/main.py
+++ app/main.py
@@ -4,25 +4,6 @@ from fastapi import FastAPI, HTTPException, Depends
 app = FastAPI(
     title="Paye ton kawa",
     description="Le café c'est la vie",
-    summary="API Produits",
+    summary="API Commandes",
     version="0.0.2",
 )
-
-@app.get("/", response_model=dict, tags=["Health Check"])
-def api_status():
-    """
-    Verifies the API status.
-
-    Returns:
-        dict: dict with the API status.
-    """
-    return {"status": "running"}
-
-@app.get("/orders/", tags=["Orders"])
-def get_orders():
-    return {"get orders": "ok"}
-
-
-@app.get("/orders/{id}", tags=["Orders"])
-def get_order():
-    return {"get order by id": "ok"}
